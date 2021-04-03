31ff31584422c119e4e3d828f7d4820d838acb67
clarissathomas <52270487+clarissathomas@users.noreply.github.com>
2021-04-03 22:44:02 +0530

    Update Question.js

--- js/Question.js
+++ js/Question.js
@@ -5,6 +5,7 @@ class Question {
     this.input1 = createInput("Enter Your Name Here....");
     this.input2 = createInput("Enter Correct Option No..");
     this.button = createButton('Submit');
+    this.reset = createButton('Reset');
     this.question = createElement('h3');
     this.option1 = createElement('h4');
     this.option2 = createElement('h4');
@@ -37,7 +38,7 @@ class Question {
     this.input1.position(150, 230);
     this.input2.position(350, 230);
     this.button.position(290, 300);
-
+    this.reset.position(750,300);
     this.button.mousePressed(()=>{
       this.title.hide();
       this.input1.hide();
@@ -50,5 +51,14 @@ class Question {
       contestant.update();
       contestant.updateCount(contestantCount);
     });
+    this.reset.mousePressed(() => {
+        contestant.updateCount(0);
+        quiz.update(0);
+  
+  
+  
+      var contestantInfoRef = database.ref('contestants');
+              contestantInfoRef.remove();
+      });
   }
 }
